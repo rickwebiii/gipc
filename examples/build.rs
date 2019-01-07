fa18d02a1b9e7dec0c32905842e0675893f56313
rickwebiii <rick.weber.iii@gmail.com>
2019-01-07 00:27:32 -0800

    Generates proto

--- examples/build.rs
+++ examples/build.rs
@@ -1,33 +1,63 @@
-use dirs::{home_dir};
+use dirs::home_dir;
 
+use std::env;
 use std::path::PathBuf;
-use std::process::{Command};
+use std::process::Command;
 
 fn main() {
-  let cargo_dir = home_dir()
-    .expect("Failed to get user directory")
-    .join(".cargo");
+    let cargo_dir = home_dir()
+        .expect("Failed to get user directory")
+        .join(".cargo")
+        .join("bin");
 
-  let grpc_rust_plugin_path = if cfg!(windows) {
-    cargo_dir.join("grpc_rust_plugin.exe")
-  } else {
-    cargo_dir.join("grpc_rust_plugin")
-  };
+    let grpc_rust_plugin_path = if cfg!(windows) {
+        cargo_dir.join("grpc_rust_plugin.exe")
+    } else {
+        cargo_dir.join("grpc_rust_plugin")
+    };
 
-  let hello_proto_path = PathBuf::from("proto")
-    .join("hello.proto");
+    let protoc_path = env::current_dir()
+        .unwrap()
+        .join("..")
+        .join("tools")
+        .join("out")
+        .join("protoc")
+        .join("bin")
+        .join("protoc");
 
-  let protoc_result = Command::new("protoc")
-    .arg("--rust_out=.")
-    .arg("--grpc_out=.")
-    .arg(format!("--plugin=protoc-gen-grpc={:?}", grpc_rust_plugin_path))
-    .arg(format!("{:?}", hello_proto_path))
-    .output()
-    .expect("Failed to start protoc");
+    let proto_dir = env::current_dir()
+      .unwrap()
+      .join("proto");
 
-  if !protoc_result.status.success() {
-    panic!("protoc failed with exit code {}:{}", protoc_result.status.code().unwrap(), String::from_utf8(protoc_result.stdout).unwrap());
-  }
+    let hello_proto_path = proto_dir
+      .join("hello.proto");
 
-  panic!("");
+    let protoc_out = env::current_dir()
+      .unwrap()
+      .join("src")
+      .join("gen");
+
+    println!("{:?}", grpc_rust_plugin_path);
+
+    let protoc_result = Command::new(protoc_path)
+        .arg(format!("--rust_out={}", protoc_out.to_string_lossy()))
+        .arg(format!("--grpc_out={}", protoc_out.to_string_lossy()))
+        .arg(format!(
+            "--plugin=protoc-gen-grpc={}",
+            grpc_rust_plugin_path.to_string_lossy()
+        ))
+        .arg(format!("--proto_path={}", proto_dir.to_string_lossy()))
+        .arg(format!("{}", hello_proto_path.to_string_lossy()))
+        .output()
+        .expect("Failed to start protoc");
+   
+    println!("protoc stdout:{}", String::from_utf8_lossy(&protoc_result.stdout));
+    println!("protoc stderr:{}", String::from_utf8_lossy(&protoc_result.stderr));
+
+    if !protoc_result.status.success() {
+        panic!(
+            "protoc failed with exit code {}",
+            protoc_result.status.code().unwrap()
+        );
+    }
 }
--- tools/build.rs
+++ tools/build.rs
@@ -1,73 +1,72 @@
-use zip::{ZipArchive};
-use zip::read::{ZipFile};
+use zip::read::ZipFile;
+use zip::ZipArchive;
 
 use std::env;
 use std::fs::{create_dir, File};
-use std::io::{BufReader, BufWriter};
 use std::io::prelude::*;
-use std::path::{Path};
+use std::io::{BufReader, BufWriter};
+use std::path::Path;
 
 const DIRECTORY: u32 = 2 << 13;
 const FILE: u32 = 2 << 14;
 
 fn main() {
-  let protoc_zip_path = if cfg!(windows) {
-    env::current_dir()
-      .unwrap()
-      .join("protoc-3.6.1-win32.zip")
-  } else {
-    panic!("TODO: support other OSs")
-  };
+    let protoc_zip_path = if cfg!(windows) {
+        env::current_dir().unwrap().join("protoc-3.6.1-win32.zip")
+    } else {
+        panic!("TODO: support other OSs")
+    };
 
-  let out_dir = env::current_dir()
-    .unwrap()
-    .join("out");
+    let out_dir = env::current_dir().unwrap().join("out");
 
-  create_dir(&out_dir);
+    create_dir(&out_dir);
 
-  println!("Reading {:?}", protoc_zip_path);
+    println!("Reading {:?}", protoc_zip_path);
 
-  unzip(&protoc_zip_path, &out_dir.join("protoc"));
+    unzip(&protoc_zip_path, &out_dir.join("protoc"));
 }
 
 fn unzip(zip_path: &Path, destination: &Path) {
-  let zip_file = File::open(zip_path).expect("Failed to read zip file.");
-  let reader = BufReader::new(zip_file);
-  let mut archive = ZipArchive::new(reader).expect("Failed to read zip file.");
+    let zip_file = File::open(zip_path).expect("Failed to read zip file.");
+    let reader = BufReader::new(zip_file);
+    let mut archive = ZipArchive::new(reader).expect("Failed to read zip file.");
 
-  create_dir(&destination);
+    create_dir(&destination);
 
-  println!("{:?} {:?}", FILE, DIRECTORY);
+    println!("{:?} {:?}", FILE, DIRECTORY);
 
-  for i in 0..archive.len() {
-    let mut file = archive.by_index(i).unwrap();
+    println!("cargo:rerun-if-changed={}", destination.to_string_lossy());
 
-    println!("{}, {:?}", file.name(), is_dir(&file));
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
 
-    let file_destination = destination.join(file.name());
+        println!("{}, {:?}", file.name(), is_dir(&file));
 
-    if is_dir(&file) {
-      println!("Creating directory {:?}", &file_destination);
-      create_dir(&file_destination);
-    } else if is_file(&file) {
-      println!("Extracting {:?}", &file_destination);
-      let out_file = File::create(&file_destination).expect(&format!("Failed to create {:?}", file_destination));
-      let mut writer = BufWriter::new(out_file);
-      let mut data: Vec<u8> = vec![];
-      
-      file.read_to_end(&mut data);
+        let file_destination = destination.join(file.name());
 
-      // TODO: set real permissions on Unix.
+        if is_dir(&file) {
+            println!("Creating directory {:?}", &file_destination);
+            create_dir(&file_destination);
+        } else if is_file(&file) {
+            println!("Extracting {:?}", &file_destination);
+            let out_file = File::create(&file_destination)
+                .expect(&format!("Failed to create {:?}", file_destination));
+            let mut writer = BufWriter::new(out_file);
+            let mut data: Vec<u8> = vec![];
 
-      writer.write(&data);
+            file.read_to_end(&mut data);
+
+            // TODO: set real permissions on Unix.
+
+            writer.write(&data);
+        }
     }
-  }
 }
 
 fn is_dir(file: &ZipFile) -> bool {
-  file.unix_mode().unwrap() & DIRECTORY != 0
+    file.unix_mode().unwrap() & DIRECTORY != 0
 }
 
 fn is_file(file: &ZipFile) -> bool {
-  file.unix_mode().unwrap() & FILE != 0
+    file.unix_mode().unwrap() & FILE != 0
 }
